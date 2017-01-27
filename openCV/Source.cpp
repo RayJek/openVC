0f8c35e52c488e0d8d6eafd2cf8235cd82be64fa
RayJek <wiecek.krystian@gmail.com>
2017-01-28 00:31:36 +0100

    panika

--- openCV/Source.cpp
+++ openCV/Source.cpp
@@ -5,13 +5,18 @@
 #include <opencv2/highgui.hpp> 
 #include <stdlib.h>
 #include <stdio.h>
+#include <math.h>
+#include <deque>
+#include "opencv/cxcore.h"
+#include <opencv2/core/types_c.h>
 
 using namespace std;
 using namespace cv;
 
 int main(int argc, char** argv) {
+
 	
-	
+
 	VideoCapture cap(-1);
 	
 
@@ -22,25 +27,35 @@ int main(int argc, char** argv) {
 		return -1;
 	}
 	do {
-
+		
+		const int MAX_NUM_OBJECTS = 50;
+		const int MAX_OBJECT_AREA = 20 * 20;
 		Mat  edges;
 		Mat frame;
 		Mat contours;
 		Mat seeline;
 		cap >> frame;
-		seeline = Mat::zeros(Size(200, 300), CV_8UC3);
-		line(frame, Point(0, 50), Point(200, 50), Scalar(0, 255, 0), 2, 8, 0);
-		imshow("norml", frame);
 
+		//rysowanie lini
+		seeline = Mat::zeros(Size(100, 100), CV_8UC3);
+		line(frame, Point(0, 50), Point(320, 50), Scalar(0, 255, 0), 4, 18, 0);
+		//standardowy obraz
+		imshow("norml", frame);
+		
+		
+
+		// znieksztalcnie
 		cvtColor(frame, edges, COLOR_BGR2GRAY);
 		GaussianBlur(edges, edges, Size(7, 7), 1.5, 1.5);
 		Canny(edges, edges, 0, 30, 3);
+		//okno 1
 		imshow("edges", edges);
 		
-		
+		//mniejsze szumy
 		Canny(frame, contours, 50, 350);
 		Mat contoursInv;
 		threshold(contours, contoursInv, 128, 255, THRESH_BINARY_INV);
+		//okno 2
 		imshow("line", contours);
 
 	} while ((waitKey(1) & 0x0ff) != 27);
