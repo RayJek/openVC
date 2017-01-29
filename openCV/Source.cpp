4be25baa2c6cad0b5be899ee1b18af5e2b7e7f4f
RayJek <wiecek.krystian@gmail.com>
2017-01-29 22:12:42 +0100

    Detekcja Lini, rysowanie Pionowe/Poziome

--- openCV/Source.cpp
+++ openCV/Source.cpp
@@ -4,7 +4,6 @@
 #include <opencv2/highgui.hpp> 
 #include <stdlib.h>
 #include <stdio.h>
-#include "opencv2/imgcodecs.hpp"
 
 
 
@@ -23,7 +22,9 @@ int main(int argc, char** argv) {
 	Mat frame;
 	namedWindow("pionowe", 1);
 	do {
+
 		Mat gray;
+
 		bool bSuccess = cap.read(frame); // wczytanie frame
 
 		if (!bSuccess) // przerwanie pêtli 
@@ -36,17 +37,27 @@ int main(int argc, char** argv) {
 		{
 			cvtColor(frame, gray, CV_BGR2GRAY);
 		}
+
 		else
 		{
 			gray = frame;
 		}
-
+		//Orginalny Obraz
+		imshow("normal", frame);
+		//zmiana obrazu na graya
 		adaptiveThreshold(~gray, gray, 255, CV_ADAPTIVE_THRESH_MEAN_C, THRESH_BINARY, 15, -2);
 		Mat vertical = gray.clone();
 		int horizontalsize = gray.cols / 30;
 
-
 		//Detekcja Pionowa
+		Mat structureOrigin = getStructuringElement(MORPH_RECT, Size(horizontalsize, 1));
+		Mat gray1 = frame;
+		erode(gray, gray1, structureOrigin, Point(-1, -1));
+		dilate(gray, gray1, structureOrigin, Point(-1, -1));
+
+		imshow("pionowe", gray1);
+
+		//Detekcja Lini Pionowa-Pozioma
 		Mat structure = getStructuringElement(MORPH_RECT, Size(horizontalsize, 1));
 
 		erode(gray, gray, structure, Point(-1, -1));
@@ -54,16 +65,11 @@ int main(int argc, char** argv) {
 
 		vector<Vec2f> lines;
 		
-		//HoughLinesP(gray, lines, 1, CV_PI / 180, 50, 50, 10);
-		
+		//HougeLinesP
 		HoughLines(gray, lines, 1, CV_PI / 180, 150, 0, 0);
 		Canny(frame, gray, 50, 200, 3);
 
-		/*for (size_t i = 0; i < lines.size(); i++)
-		{
-		}*/
-
-		// draw lines
+		// rysownaie lini 
 		for (size_t i = 0; i < lines.size(); i++)
 		{
 			float rho = lines[i][0], theta = lines[i][1];
@@ -77,9 +83,7 @@ int main(int argc, char** argv) {
 			line(gray, pt1, pt2, Scalar(255, 255, 255), 3, CV_AA);
 		}
 
-		//imshow("Pionowe", gray);
-		imshow("pionowe", gray);
-
+		imshow("Detekcja Lini", gray);
 
 		//Detekcja Pozioma
 		int verticalsize = vertical.rows / 30;
@@ -89,29 +93,7 @@ int main(int argc, char** argv) {
 		erode(vertical, vertical, verticalStructure, Point(-1, -1));
 		dilate(vertical, vertical, verticalStructure, Point(-1, -1));
 
-		HoughLines(frame, lines, 1, CV_PI / 180, 150, 0, 0);
-		Canny(frame, vertical, 50, 200, 3);
-
-		for (size_t i = 0; i < lines.size(); i++)
-		{
-			float rho = lines[i][0], theta = lines[i][1];
-			Point pt1, pt2;
-			double a = cos(theta), b = sin(theta);
-			double x0 = a*rho, y0 = b*rho;
-			pt1.x = cvRound(x0 + 1000 * (-b));
-			pt1.y = cvRound(y0 + 1000 * (a));
-			pt2.x = cvRound(x0 - 1000 * (-b));
-			pt2.y = cvRound(y0 - 1000 * (a));
-			line(gray, pt1, pt2, Scalar(255, 255, 255), 3, CV_AA);
-		}
-
-
-
 		imshow("Poziome", vertical);
-		
-
-
-
 
 	} while ((waitKey(1) & 0x0ff) != 27);
 
